d4f32bd351c92c3910f09530d865b2f228fcf9e1
danderson100 <danderson7790@gmail.com>
2021-01-11 08:33:03 -0700

    Added buttons and finishing layout

---
+++ script.js
@@ -0,0 +1,7 @@
+const calcContainer = document.querySelector('.calc-container');
+
+// for (i = 0; i < 25; i++) {
+//     let cell = document.createElement('div');
+//     cell.classList.add("grid-item");
+//     calcContainer.appendChild(cell);
+// }
